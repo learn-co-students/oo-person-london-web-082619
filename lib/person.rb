a285a8e3f38ec8f8522a2b6f51d2e5b1245be90d
Angus Morrison <contact@angus-morrison.com>
2019-08-29 12:46:28 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
+
 class Person
     attr_reader :name, :hygiene, :happiness
     attr_accessor :bank_account
@@ -44,35 +45,31 @@ class Person
     end
 
     def take_bath
-        self.hygiene=(self.hygiene + 4)
+        self.hygiene += 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.happiness=(self.happiness + 2)
-        self.hygiene=(self.hygiene - 3)
+        self.happiness += 2
+        self.hygiene -= 3
         "♪ another one bites the dust ♫"
     end
 
     def call_friend(callee)
-        self.happiness=(self.happiness + 3)
-        callee.happiness=(callee.happiness + 3)
+        [callee, self].each { | person | person.happiness += 3 }
         "Hi #{callee.name}! It's #{self.name}. How are you?"
     end
 
     def start_conversation(person, topic)
         if topic == "politics"
-            self.happiness=(self.happiness - 2)
-            person.happiness=(person.happiness - 2)
+            [person, self].each { | human | human.happiness -= 2 }
             return "blah blah partisan blah lobbyist"
         elsif topic == "weather"
-            self.happiness=(self.happiness + 1)
-            person.happiness=(person.happiness + 1)
+            [person, self].each { | human | human.happiness += 1 }
             return "blah blah sun blah rain"
         else
             return "blah blah blah blah blah"
         end
     end
-
 
 end
