30110b6040f8f75607bd6b2084f83076cb006139
Michelle Van <mjtvan@gmail.com>
2019-08-29 12:41:26 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -4,11 +4,18 @@ class Person
     attr_reader :name 
 
 
-    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+  #  def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+   #     @name = name 
+   #     @bank_account = bank_account
+    #    @happiness = happiness 
+     #   @hygiene = hygiene
+  #  end 
+
+    def initialize(name)
         @name = name 
-        @bank_account = bank_account
-        @happiness = happiness 
-        @hygiene = hygiene
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end 
 
     def happiness= (happiness)
